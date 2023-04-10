2a8a55e69cee8be3492560248c34c21132bc5ae9
AuroraKall <129622793+AuroraKall@users.noreply.github.com>
2023-04-10 12:15:10 +0200

    Visa Kund

--- src/Hattmakarna/HanteraKundInfoFonster.java
+++ src/Hattmakarna/HanteraKundInfoFonster.java
@@ -1,5 +1,6 @@
 package Hattmakarna;
 
+import java.util.HashMap;
 import javax.swing.JOptionPane;
 import oru.inf.InfDB;
 import oru.inf.InfException;
@@ -22,19 +23,18 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jÄndratKundNamn = new javax.swing.JTextField();
-        jÄndratKundTel = new javax.swing.JTextField();
         jÄndraKundMejl = new javax.swing.JLabel();
-        jAngeLojalitetsKund = new javax.swing.JTextField();
         jLojalitetsKund = new javax.swing.JLabel();
         jÄndraKundTel = new javax.swing.JLabel();
-        jÄndradKundMejl = new javax.swing.JTextField();
-        jÄndradKundAdress = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
         jÄndraKundNamn = new javax.swing.JLabel();
         jÄndraKundAdress = new javax.swing.JLabel();
         jAngivetKundID = new javax.swing.JTextField();
         jKundIDKnapp = new javax.swing.JButton();
+        lblKundNamn = new javax.swing.JLabel();
+        lblKundTel = new javax.swing.JLabel();
+        lblKundAdress = new javax.swing.JLabel();
+        lblKundEmail = new javax.swing.JLabel();
+        lblKundLojalitet = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -43,24 +43,11 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
 
         jLabel2.setText("Ange KundID");
 
-        jÄndratKundNamn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jÄndratKundNamnActionPerformed(evt);
-            }
-        });
-
         jÄndraKundMejl.setText("E-postadress");
 
         jLojalitetsKund.setText("Lojalitetskund");
 
         jÄndraKundTel.setText("Telefonnummer");
-
-        jButton1.setText("Ändra kundinformation");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         jÄndraKundNamn.setText("Namn");
 
@@ -92,21 +79,20 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
                     .addComponent(jÄndraKundTel)
                     .addComponent(jLabel2)
                     .addComponent(jÄndraKundMejl))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 150, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jÄndratKundTel, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndradKundAdress, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndradKundMejl, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jAngeLojalitetsKund, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndratKundNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jAngivetKundID, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jKundIDKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                        .addComponent(jKundIDKnapp, javax.swing.GroupLayout.PREFERRED_SIZE, 67, Short.MAX_VALUE))
+                    .addComponent(lblKundNamn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblKundTel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblKundAdress, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblKundEmail, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblKundLojalitet, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(158, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(148, 148, 148))
         );
@@ -117,33 +103,31 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jKundIDKnapp, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jKundIDKnapp, javax.swing.GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jLabel2)
                         .addComponent(jAngivetKundID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jÄndratKundNamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndraKundNamn))
+                    .addComponent(jÄndraKundNamn)
+                    .addComponent(lblKundNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jÄndratKundTel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndraKundTel))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jÄndradKundAdress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndraKundAdress))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jÄndradKundMejl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jÄndraKundMejl))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jAngeLojalitetsKund, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLojalitetsKund))
-                .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addGap(16, 16, 16))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jÄndraKundTel)
+                    .addComponent(lblKundTel, javax.swing.GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jÄndraKundAdress, javax.swing.GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+                    .addComponent(lblKundAdress, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jÄndraKundMejl)
+                    .addComponent(lblKundEmail, javax.swing.GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLojalitetsKund)
+                    .addComponent(lblKundLojalitet, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(56, 56, 56))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -158,7 +142,7 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(50, Short.MAX_VALUE))
+                .addContainerGap(72, Short.MAX_VALUE))
         );
 
         pack();
@@ -168,61 +152,51 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jAngivetKundIDActionPerformed
 
-    private void jÄndratKundNamnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jÄndratKundNamnActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jÄndratKundNamnActionPerformed
-
     private void jKundIDKnappActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jKundIDKnappActionPerformed
-      
+    if(jAngivetKundID.getText().isEmpty()){
+        JOptionPane.showMessageDialog(null, "Vänligen fyll i ett KundID");
+    }
+    setInfo();      
     }//GEN-LAST:event_jKundIDKnappActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-      if(jAngivetKundID.getText().isEmpty()){
-           JOptionPane.showMessageDialog(null, "Vänligen fyll i ett Kund ID och det du vill ändra!");
-       } else{
-      }
-                String kundid = jAngivetKundID.getText();
-                String namn = jÄndratKundNamn.getText();
-                String telefonnummer = jÄndraKundTel.getText();
-                String gatuadress = jÄndraKundAdress.getText();
-                if (jÄndratKundNamn.getText().isEmpty() == false) {
-                    try {
-                        JOptionPane.showMessageDialog(null, "Namn har ändrats!");
-                        idb.update("UPDATE kund SET Namn = '" + namn + "' WHERE Kundid = " + jAngivetKundID);
-                    } catch (InfException error) {
-                        JOptionPane.showMessageDialog(null, "Något gick fel!");
-                        System.out.println("Internt felmedelande" + error.getMessage());
-                    }
-                }
-                if (jÄndraKundTel.getText().isEmpty() == false) {
-                    try {
-                        JOptionPane.showMessageDialog(null, "Telefonnummer har ändrats!");
-                        idb.update("UPDATE kund SET Telefonnummer = '" + telefonnummer + "' WHERE Kundid = " + jÄndraKundTel);
-                    } catch (InfException error) {
-                        JOptionPane.showMessageDialog(null, "Något gick fel!");
-                        System.out.println("Internt felmedelande" + error.getMessage());
-                    }
-                }
-                 if (jÄndraKundAdress.getText().isEmpty() == false) {
-                    try {
-                        JOptionPane.showMessageDialog(null, "Gatuadress har ändrats!");
-                        idb.update("UPDATE kund SET gatuadress = '" + gatuadress + "' WHERE kundid = " + jÄndraKundAdress);
-                    } catch (InfException error) {
-                        JOptionPane.showMessageDialog(null, "Något gick fel!");
-                        System.out.println("Internt felmedelande" + error.getMessage());
-                    }
-                }
-                 
-               
-    }//GEN-LAST:event_jButton1ActionPerformed
+    
+     public HashMap getInfoKund() {
 
+        HashMap<String, String> info = new HashMap<>();
+        try {
 
+            info = idb.fetchRow("SELECT KundID, Kund.Namn, Kund.Adress, Kund.Telefonnummer, Kund.Lojalitet, Kund.Email FROM Kund WHERE KundID = '" + jAngivetKundID + "';");
+
+        }catch (InfException ex) {
+            JOptionPane.showMessageDialog(null, "Något gick fel!");
+
+        }
+        return info;
+    }
+    
+    
+ public void setInfo() {
+        HashMap<String, String> info;
+        info = getInfoKund();
+
+        String KundID = info.get("KundID");
+        String namn = info.get("Kund.Namn");
+        String adress = info.get("Kund.Adress");
+        String telefon = info.get("Kund.Telefonnummer");
+        String lojalitet = info.get("Kund.Lojalitet");
+        String email = info.get("Kund.Email");
+
+        lblKundNamn.setText("");
+        lblKundTel.setText("");
+        lblKundAdress.setText("");
+        lblKundEmail.setText(namn);
+        lblKundLojalitet.setText(telefon);
+ 
+
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField jAngeLojalitetsKund;
     private javax.swing.JTextField jAngivetKundID;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jKundIDKnapp;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -232,9 +206,10 @@ public class HanteraKundInfoFonster extends javax.swing.JFrame {
     private javax.swing.JLabel jÄndraKundMejl;
     private javax.swing.JLabel jÄndraKundNamn;
     private javax.swing.JLabel jÄndraKundTel;
-    private javax.swing.JTextField jÄndradKundAdress;
-    private javax.swing.JTextField jÄndradKundMejl;
-    private javax.swing.JTextField jÄndratKundNamn;
-    private javax.swing.JTextField jÄndratKundTel;
+    private javax.swing.JLabel lblKundAdress;
+    private javax.swing.JLabel lblKundEmail;
+    private javax.swing.JLabel lblKundLojalitet;
+    private javax.swing.JLabel lblKundNamn;
+    private javax.swing.JLabel lblKundTel;
     // End of variables declaration//GEN-END:variables
 }
