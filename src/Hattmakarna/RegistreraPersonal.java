54b16a5bf59688e74359400a8b7389d53b1bbc7e
AuroraKall <129622793+AuroraKall@users.noreply.github.com>
2023-04-13 10:25:51 +0200

    .

--- src/Hattmakarna/RegistreraPersonal.java
+++ src/Hattmakarna/RegistreraPersonal.java
@@ -26,17 +26,27 @@ public class RegistreraPersonal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblRegKund = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblRegKund.setText("Registrera ny personal");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(157, 157, 157)
+                .addComponent(lblRegKund)
+                .addContainerGap(126, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(lblRegKund)
+                .addContainerGap(248, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +88,6 @@ public class RegistreraPersonal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblRegKund;
     // End of variables declaration//GEN-END:variables
 }
