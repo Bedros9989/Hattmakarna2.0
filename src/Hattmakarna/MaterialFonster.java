a19084d57c64a1380690061f02a6effc2b2a6710
RilaMareein <121014746+RilaMareein@users.noreply.github.com>
2023-04-14 10:07:20 +0200

    HanteraMaterial

--- src/Hattmakarna/MaterialFonster.java
+++ src/Hattmakarna/MaterialFonster.java
@@ -17,6 +17,7 @@ public class MaterialFonster extends javax.swing.JFrame {
     private void initComponents() {
 
         btnRegMaterial = new javax.swing.JButton();
+        jBHanteraMaterial = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -27,13 +28,22 @@ public class MaterialFonster extends javax.swing.JFrame {
             }
         });
 
+        jBHanteraMaterial.setText("Hantera material");
+        jBHanteraMaterial.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBHanteraMaterialActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(126, Short.MAX_VALUE)
-                .addComponent(btnRegMaterial)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jBHanteraMaterial, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnRegMaterial, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(122, 122, 122))
         );
         layout.setVerticalGroup(
@@ -41,7 +51,9 @@ public class MaterialFonster extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(71, 71, 71)
                 .addComponent(btnRegMaterial, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(186, Short.MAX_VALUE))
+                .addGap(54, 54, 54)
+                .addComponent(jBHanteraMaterial, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(90, Short.MAX_VALUE))
         );
 
         pack();
@@ -52,8 +64,13 @@ public class MaterialFonster extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_btnRegMaterialActionPerformed
 
+    private void jBHanteraMaterialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBHanteraMaterialActionPerformed
+        new HanteraMaterial(idb).setVisible(true);
+    }//GEN-LAST:event_jBHanteraMaterialActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnRegMaterial;
+    private javax.swing.JButton jBHanteraMaterial;
     // End of variables declaration//GEN-END:variables
 }
