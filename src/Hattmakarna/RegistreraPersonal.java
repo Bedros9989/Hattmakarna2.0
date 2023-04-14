006fa0f3306fe4e4ecb396ae8b43831b211607c2
Alvalofgren <alvalofg@gmail.com>
2023-04-14 14:25:13 +0200

    fixat i registrera personal

--- src/Hattmakarna/RegistreraPersonal.java
+++ src/Hattmakarna/RegistreraPersonal.java
@@ -21,26 +21,75 @@ public class RegistreraPersonal extends javax.swing.JFrame {
     private void initComponents() {
 
         lblRegKund = new javax.swing.JLabel();
+        jPersonalNamn = new javax.swing.JLabel();
+        jPersonalLosen = new javax.swing.JLabel();
+        jPersonalTelefon = new javax.swing.JLabel();
+        jPersonalTimpris = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        lblRegKund.setFont(new java.awt.Font("Calibri", 0, 22)); // NOI18N
         lblRegKund.setText("Registrera ny personal");
+
+        jPersonalNamn.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jPersonalNamn.setText("Namn");
+
+        jPersonalLosen.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jPersonalLosen.setText("Lösenord");
+
+        jPersonalTelefon.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jPersonalTelefon.setText("Telefonnummer");
+
+        jPersonalTimpris.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jPersonalTimpris.setText("Timpris");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(157, 157, 157)
-                .addComponent(lblRegKund)
-                .addContainerGap(126, Short.MAX_VALUE))
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPersonalNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPersonalLosen, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblRegKund)
+                    .addComponent(jPersonalTelefon, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPersonalTimpris, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
+                    .addComponent(jTextField2)
+                    .addComponent(jTextField3)
+                    .addComponent(jTextField4))
+                .addGap(82, 82, 82))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblRegKund)
+                        .addGap(35, 35, 35)
+                        .addComponent(jPersonalNamn))
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPersonalLosen)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPersonalTelefon, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(36, 36, 36)
-                .addComponent(lblRegKund)
-                .addContainerGap(248, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPersonalTimpris))
+                .addContainerGap(66, Short.MAX_VALUE))
         );
 
         pack();
@@ -48,6 +97,14 @@ public class RegistreraPersonal extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jPersonalLosen;
+    private javax.swing.JLabel jPersonalNamn;
+    private javax.swing.JLabel jPersonalTelefon;
+    private javax.swing.JLabel jPersonalTimpris;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     private javax.swing.JLabel lblRegKund;
     // End of variables declaration//GEN-END:variables
 }
