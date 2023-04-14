f9b6d4a314003e036075243b33319ef9a3f673d4
Alvalofgren <alvalofg@gmail.com>
2023-04-14 15:34:42 +0200

    har ordnat med strukturen i hanteraPersonalFonster

--- src/Hattmakarna/HanteraPersonalFonster.java
+++ src/Hattmakarna/HanteraPersonalFonster.java
@@ -22,32 +22,140 @@ public class HanteraPersonalFonster extends javax.swing.JFrame {
     private void initComponents() {
 
         lblHanteraPersonal = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jÄndraPersonalID = new javax.swing.JLabel();
+        jÄndraPersonalNamn = new javax.swing.JLabel();
+        jÄndraPersonalLosen = new javax.swing.JLabel();
+        jÄndraPersonalTelefon = new javax.swing.JLabel();
+        jÄndraPersonalTimpris = new javax.swing.JLabel();
+        ID = new javax.swing.JTextField();
+        Namn = new javax.swing.JTextField();
+        Losenord = new javax.swing.JTextField();
+        Telefonnummer = new javax.swing.JTextField();
+        Timpris = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        lblHanteraPersonal.setFont(new java.awt.Font("Calibri", 0, 22)); // NOI18N
         lblHanteraPersonal.setText("Hantera personal information");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jÄndraPersonalID.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jÄndraPersonalID.setText("PersonalID");
+
+        jÄndraPersonalNamn.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jÄndraPersonalNamn.setText("Namn");
+
+        jÄndraPersonalLosen.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jÄndraPersonalLosen.setText("Lösenord");
+
+        jÄndraPersonalTelefon.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jÄndraPersonalTelefon.setText("Telefonnummer");
+
+        jÄndraPersonalTimpris.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jÄndraPersonalTimpris.setText("Timpris");
+
+        jButton1.setText("Ändra");
+
+        jButton2.setText("Spara");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(116, 116, 116)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(129, Short.MAX_VALUE)
                 .addComponent(lblHanteraPersonal)
-                .addContainerGap(129, Short.MAX_VALUE))
+                .addGap(122, 122, 122))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jÄndraPersonalTimpris, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jÄndraPersonalTelefon, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jÄndraPersonalLosen, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jÄndraPersonalNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jÄndraPersonalID, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(91, 91, 91))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jComboBox1, 0, 181, Short.MAX_VALUE)
+                        .addComponent(ID)
+                        .addComponent(Namn)
+                        .addComponent(Losenord)
+                        .addComponent(Telefonnummer)
+                        .addComponent(Timpris)))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(lblHanteraPersonal)
-                .addContainerGap(262, Short.MAX_VALUE))
+                .addGap(26, 26, 26)
+                .addComponent(lblHanteraPersonal, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jÄndraPersonalID, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jÄndraPersonalNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Namn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jÄndraPersonalLosen, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Losenord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jÄndraPersonalTelefon, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Telefonnummer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jÄndraPersonalTimpris, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Timpris, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(18, 18, 18))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField ID;
+    private javax.swing.JTextField Losenord;
+    private javax.swing.JTextField Namn;
+    private javax.swing.JTextField Telefonnummer;
+    private javax.swing.JTextField Timpris;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jÄndraPersonalID;
+    private javax.swing.JLabel jÄndraPersonalLosen;
+    private javax.swing.JLabel jÄndraPersonalNamn;
+    private javax.swing.JLabel jÄndraPersonalTelefon;
+    private javax.swing.JLabel jÄndraPersonalTimpris;
     private javax.swing.JLabel lblHanteraPersonal;
     // End of variables declaration//GEN-END:variables
 }
