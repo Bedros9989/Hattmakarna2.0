20685a94f9d99e7875bc38502f312447049b3fa4
oliverfreij <oliverfreij@gmail.com>
2023-04-14 14:58:12 +0200

    hanterahatt

--- src/Hattmakarna/ValjBild.java
+++ src/Hattmakarna/ValjBild.java
@@ -1,4 +1,3 @@
-
 package Hattmakarna;
 
 import java.awt.Image;
@@ -15,17 +14,17 @@ import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 public class ValjBild extends javax.swing.JFrame {
 
-    private ImageIcon format=null;
-    private String fname=null;
-    int s=0;
-    public byte[] pimage=null;
-    Connection conn=null;
-    
+    private ImageIcon format = null;
+    private String fname = null;
+    int s = 0;
+    public byte[] pimage = null;
+    Connection conn = null;
+
     public ValjBild() {
         initComponents();
         this.setLocationRelativeTo(null);
         ValjBild.this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        conn=ValjBild.DBConnect.connect();
+        conn = ValjBild.DBConnect.connect();
     }
 
 
@@ -103,92 +102,87 @@ public class ValjBild extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void sparaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sparaActionPerformed
-        
+
         JFileChooser filechooser = new JFileChooser();
         filechooser.showOpenDialog(null);
-        File f=filechooser.getSelectedFile();
-        fname=f.getAbsolutePath();
-        ImageIcon micon=new ImageIcon(fname);
-        
-        try{
-            
-            File image=new File(fname);
-            FileInputStream fis=new FileInputStream(image);
-            ByteArrayOutputStream baos=new ByteArrayOutputStream();
-            byte[] buf=new byte[1024];
-            for(int readnum; (readnum=fis.read(buf)) !=-1;){
-                
-                baos.write(buf,0,readnum);
+        File f = filechooser.getSelectedFile();
+        fname = f.getAbsolutePath();
+        ImageIcon micon = new ImageIcon(fname);
+
+        try {
+
+            File image = new File(fname);
+            FileInputStream fis = new FileInputStream(image);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            for (int readnum; (readnum = fis.read(buf)) != -1;) {
+
+                baos.write(buf, 0, readnum);
             }
-            pimage=baos.toByteArray();
-            jLabel1.setIcon(resizeImage(fname,buf));
-            
-        }catch (Exception e){
-            
-            
+            pimage = baos.toByteArray();
+            jLabel1.setIcon(resizeImage(fname, buf));
+
+        } catch (Exception e) {
+
         }
-        
-        
+
+
     }//GEN-LAST:event_sparaActionPerformed
 
     private void väljBildActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_väljBildActionPerformed
-        
-        int resultat = JOptionPane.showConfirmDialog(null, "Är du nöjd med bilden du har valt?", "Bekräfta bild", JOptionPane.YES_NO_OPTION);
-                
-                if(resultat == JOptionPane.YES_OPTION){
-                    RegistreraHattFonster hatt = getBildForm();
-                        hatt.hämtabild(pimage);
-                        dispose();
-                    } 
 
+        int resultat = JOptionPane.showConfirmDialog(null, "Är du nöjd med bilden du har valt?", "Bekräfta bild", JOptionPane.YES_NO_OPTION);
+
+        if (resultat == JOptionPane.YES_OPTION) {
+            RegistreraHattFonster hatt = getBildForm();
+            hatt.hämtabild(pimage);
+            dispose();
+        }
     }//GEN-LAST:event_väljBildActionPerformed
 
-    private RegistreraHattFonster getBildForm(){
-        
+    private RegistreraHattFonster getBildForm() {
+
         RegistreraHattFonster form = null;
-        for (Window window : Window.getWindows()){
-            if (window instanceof RegistreraHattFonster){
+        for (Window window : Window.getWindows()) {
+            if (window instanceof RegistreraHattFonster) {
                 form = (RegistreraHattFonster) window;
                 break;
             }
-            
+
         }
         return form;
     }
-    
-    public class DBConnect {   
-    public static Connection connect()
-        {
-     Connection con=null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");                                
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hattmakare","hattuser","hattkey");   
-            }       
-            catch (Exception e) 
-        {
-        System.out.println("inter.DBConnect.connect()");
+
+    public class DBConnect {
+
+        public static Connection connect() {
+            Connection con = null;
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hattmakare", "hattuser", "hattkey");
+            } catch (Exception e) {
+                System.out.println("inter.DBConnect.connect()");
+            }
+            return con;
         }
-          return con;
-        } 
     }
-    
-    public ImageIcon resizeImage(String imagePath, byte[] pic){
-          
-        ImageIcon myImage=null;
-        if(imagePath !=null)
-        {
-        myImage=new ImageIcon(imagePath);
-        
-        }else{
-         myImage=new ImageIcon(pic);
+
+    public ImageIcon resizeImage(String imagePath, byte[] pic) {
+
+        ImageIcon myImage = null;
+        if (imagePath != null) {
+            myImage = new ImageIcon(imagePath);
+
+        } else {
+            myImage = new ImageIcon(pic);
         }
-                
-        Image img=myImage.getImage();
-        Image img2=img.getScaledInstance(jLabel1.getHeight(),    jLabel1.getWidth(),  Image.SCALE_SMOOTH);
-        ImageIcon image=new ImageIcon(img2);
+
+        Image img = myImage.getImage();
+        Image img2 = img.getScaledInstance(jLabel1.getHeight(), jLabel1.getWidth(), Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(img2);
         return image;
     }
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
