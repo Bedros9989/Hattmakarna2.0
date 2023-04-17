a45ef6a35e8948d1540195ec1ebd357920b9521e
jesgus <solsaxen@hotmail.com>
2023-04-17 20:05:39 +0200

    jobbat på visa tillgängligt material-fönstret

--- src/Hattmakarna/SeMaterialLager.java
+++ src/Hattmakarna/SeMaterialLager.java
@@ -4,7 +4,12 @@
  */
 package Hattmakarna;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import oru.inf.InfDB;
+import oru.inf.InfException;
 
 /**
  *
@@ -13,6 +18,7 @@ import oru.inf.InfDB;
 public class SeMaterialLager extends javax.swing.JFrame {
 
     private InfDB idb;
+    DefaultTableModel model = new DefaultTableModel();
     /**
      * Creates new form SeMaterialLager
      */
@@ -21,6 +27,15 @@ public class SeMaterialLager extends javax.swing.JFrame {
         this.idb = idb;
      
         this.setLocationRelativeTo(null);
+        tabell.setModel(model);
+        model.addColumn("MaterialID");
+        model.addColumn("Materialnamn");
+        model.addColumn("Antal");
+        model.addColumn("Meter");
+        model.addColumn("Kvadratmeter");
+        tabell.setDefaultEditor(Object.class, null);
+        setMaterialTable2();
+        btnUppdatera.setVisible(false);
     }
 
     /**
@@ -32,43 +47,45 @@ public class SeMaterialLager extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jToggleButton1 = new javax.swing.JToggleButton();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
+        tabell = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        btnHanteraMaterial = new javax.swing.JButton();
+        btnUppdatera = new javax.swing.JButton();
+
+        jToggleButton1.setText("jToggleButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tabell.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null},
-                {null, null},
-                {null, null},
-                {null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "MaterialID", "Kategori"
+                "MaterialID", "Materialnamn", "Antal", "Meter", "Kvadratmeter"
             }
         ));
-        jScrollPane2.setViewportView(jTable1);
+        jScrollPane2.setViewportView(tabell);
 
-        jButton1.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        jButton1.setText("Visa Material");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 24)); // NOI18N
+        jLabel1.setText("Aktuellt materialsaldo");
+
+        btnHanteraMaterial.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        btnHanteraMaterial.setText(" Hantera information om material");
+        btnHanteraMaterial.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnHanteraMaterialActionPerformed(evt);
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel1.setText("Se materialsaldo");
-
-        jButton2.setFont(new java.awt.Font("Segoe UI", 2, 12)); // NOI18N
-        jButton2.setText(" Hantera information om material");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnUppdatera.setText("Uppdatera saldo");
+        btnUppdatera.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnUppdateraActionPerformed(evt);
             }
         });
 
@@ -76,52 +93,106 @@ public class SeMaterialLager extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(482, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addGap(13, 13, 13))
             .addGroup(layout.createSequentialGroup()
                 .addGap(69, 69, 69)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 384, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton1)))
-                .addGap(0, 264, Short.MAX_VALUE))
+                        .addComponent(jLabel1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnHanteraMaterial)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 630, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(502, 502, 502)
+                                    .addComponent(btnUppdatera))))
+                        .addGap(0, 97, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(62, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
-                .addGap(18, 18, 18)
+                .addGap(32, 32, 32)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
+                .addComponent(btnHanteraMaterial, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(50, 50, 50))
+                .addComponent(btnUppdatera)
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void btnHanteraMaterialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHanteraMaterialActionPerformed
+        btnUppdatera.setVisible(true);
+    }//GEN-LAST:event_btnHanteraMaterialActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void btnUppdateraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUppdateraActionPerformed
+       setMaterialTable2();
+    }//GEN-LAST:event_btnUppdateraActionPerformed
 
+        
+            private void setMaterialTable2() {
 
+        model.setRowCount(0);
+
+        try {
+            
+                  ArrayList<HashMap<String, String>> antalLager = idb.fetchRows("select material.MaterialID, Material.Materialnamn, Antal from material " +
+"join Antalvara on Material.MaterialID = antalvara.MaterialID");
+
+            for (HashMap<String, String> antal : antalLager) {
+                Object[] antalData = {
+                    antal.get("MaterialID"),
+                    antal.get("Materialnamn"),
+                antal.get("Antal"),};
+                model.addRow(antalData);
+        
+            ArrayList<HashMap<String, String>> meterLager = idb.fetchRows("select material.MaterialID, Material.Materialnamn, meter from material"+
+" join Metervara on material.MaterialID=metervara.MaterialID");
+
+            for (HashMap<String, String> material : meterLager) {
+                Object[] materialData = {
+                    material.get("MaterialID"),
+                    material.get("Materialnamn"),
+                    material.get("Antal"),
+                material.get("Meter"),};
+                model.addRow(materialData);
+            }
+        
+                // Bortkommenterad då den inte är klar, men ska vara för kvadratmetervaror nedan:
+                
+//                 ArrayList<HashMap<String, String>> kvadratLager = idb.fetchRows();
+//
+//            for (HashMap<String, String> kvadrat : kvadratLager) {
+//                Object[] materialData = {
+//                    kvadrat.get("MaterialID"),
+//                    kvadrat.get("Materialnamn"),
+//                    kvadrat.get("Antal"),
+//                kvadrat.get("Meter"),
+//                model.addRow(materialData);
+            
+                      
+            
+//            }
+            }
+            
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, "Databasfel!");
+            System.out.println("Databasfel: " + e);
+        }
+    
+            }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnHanteraMaterial;
+    private javax.swing.JButton btnUppdatera;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JTable tabell;
     // End of variables declaration//GEN-END:variables
 }
