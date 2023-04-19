2e58fff7841e27a2777a0ad429163b17ff0996a7
JossanGus <105631856+JossanGus@users.noreply.github.com>
2023-04-19 09:53:03 +0200

    mås

--- src/Hattmakarna/SeMaterialLager.java
+++ src/Hattmakarna/SeMaterialLager.java
@@ -153,7 +153,7 @@ public class SeMaterialLager extends javax.swing.JFrame {
                     antal.get("Materialnamn"),
                 antal.get("Antal"),};
                 model.addRow(antalData);
-        
+            }
             ArrayList<HashMap<String, String>> meterLager = idb.fetchRows("select material.MaterialID, Material.Materialnamn, meter from material"+
 " join Metervara on material.MaterialID=metervara.MaterialID");
 
@@ -178,20 +178,21 @@ public class SeMaterialLager extends javax.swing.JFrame {
                      kvadrat.get("Antal"),
                       kvadrat.get("Meter"),
                      kvadrat.get("Kvadratmeter"),
+                
                };
                 model.addRow(materialData);
             
-                      
-            
+                     
             }
             }
             
-        } catch (InfException e) {
+         catch (InfException e) {
             JOptionPane.showMessageDialog(null, "Databasfel!");
             System.out.println("Databasfel: " + e);
         }
     
             }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnHanteraMaterial;
